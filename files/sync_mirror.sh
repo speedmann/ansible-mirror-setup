71d2dcf6f059223892d49edf96c22ba0de1ed564
speedmann <speedmann@speedmann.de>
2017-12-12 00:57:30 +0100

    Initial version

---
+++ files/sync_mirror.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+fatal() {
+  echo "$1"
+  exit 1
+}
+
+warn() {
+  echo "$1"
+}
+
+# Find a source mirror near you which supports rsync on
+# https://launchpad.net/ubuntu/+cdmirrors
+# rsync://<iso-country-code>.rsync.releases.ubuntu.com/releases should always work
+RSYNCSOURCE=rsync://de.rsync.releases.ubuntu.com/releases
+
+# Define where you want the mirror-data to be on your mirror
+BASEDIR=/var/www/mirror.tuxcall.de/ubuntu/
+
+if [ ! -d ${BASEDIR} ]; then
+  warn "${BASEDIR} does not exist yet, trying to create it..."
+  mkdir -p ${BASEDIR} || fatal "Creation of ${BASEDIR} failed."
+fi
+
+rsync --verbose --recursive --times --links --safe-links --hard-links \
+  --stats --delete-after \
+  ${RSYNCSOURCE} ${BASEDIR} || fatal "Failed to rsync from ${RSYNCSOURCE}."
+
+date -u > ${BASEDIR}/.trace/$(hostname -f)
+
